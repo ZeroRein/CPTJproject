2e2a7b3977076aa21afda45ae43c6b0c41a193a8
ZeroRein <ktC23A31E0007@edu.kyoto-tech.ac.jp>
2024-06-08 17:56:49 +0900

    a

--- notice.py
+++ notice.py
@@ -1,1 +1,12 @@
-import tkinter
+import sys
+import tkinter as tk
+import tkinter.font as f
+
+root = tk.Tk()
+root.title(u"Software Title")
+root.geometry("400x300")
+
+Static1 = tk.Label(text=u'データ消えたけど今どんな気持ち？',font=("MSゴシック", "20",))
+
+Static1.pack()
+root.mainloop()
---
+++ run_python_script.bat
@@ -0,0 +1,2 @@
+@echo off
+python C:\Users\ktc\Documents\23a31e0007\CPTJproject\notice.py
